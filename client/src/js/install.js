63f0019ac262978bd758b01f0ac2f1de6460f037
Christopher Peret <cperet89@gmail.com>
2023-07-22 15:22:10 -0700

    todos in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,28 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    butInstall.removeAttribute('hidden');
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+    promptEvent.prompt();
+    const result = await promptEvent.userChoice;
+    if (result.outcome === 'accepted') {
+        console.log('PWA setup accepted');
+    } else {
+        console.log('PWA setup rejected');
+    }
+    window.deferredPrompt = null;
+    butInstall.setAttribute('hidden', true);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+    console.log('PWA setup installed');
+});
